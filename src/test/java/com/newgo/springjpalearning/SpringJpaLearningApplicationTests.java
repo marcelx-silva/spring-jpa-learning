917720abf1355a0a5a1b1f91d0f7f3caae1aa7ca
marcelx-silva <thiago.silvamarcelo56@gmail.com>
2022-12-21 21:50:11 -0300

    initial commit

---
+++ src/test/java/com/newgo/springjpalearning/SpringJpaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newgo.springjpalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
